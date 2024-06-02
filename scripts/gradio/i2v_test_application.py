ac8bf9d84c9bfbdfc491d6d19c274f5248f21745
Nilaier <nilaiermusic@gmail.com>
2024-06-03 02:50:22 +0300

    test with 2 GPUs

--- scripts/gradio/i2v_test_application.py
+++ scripts/gradio/i2v_test_application.py
@@ -11,28 +11,29 @@ from pytorch_lightning import seed_everything
 from einops import rearrange
 
 class Image2Video():
-    def __init__(self,result_dir='./tmp/',gpu_num=1,resolution='256_256') -> None:
-        self.resolution = (int(resolution.split('_')[0]), int(resolution.split('_')[1])) #hw
+    def __init__(self, result_dir='./tmp/', gpu_num=2, resolution='256_256') -> None:
+        self.resolution = (int(resolution.split('_')[0]), int(resolution.split('_')[1])) # hw
         self.download_model()
         
         self.result_dir = result_dir
         if not os.path.exists(self.result_dir):
             os.mkdir(self.result_dir)
-        ckpt_path='checkpoints/tooncrafter_'+resolution.split('_')[1]+'_interp_v1/model.ckpt'
-        config_file='configs/inference_'+resolution.split('_')[1]+'_v1.0.yaml'
+        ckpt_path = 'checkpoints/tooncrafter_' + resolution.split('_')[1] + '_interp_v1/model.ckpt'
+        config_file = 'configs/inference_' + resolution.split('_')[1] + '_v1.0.yaml'
         config = OmegaConf.load(config_file)
         model_config = config.pop("model", OmegaConf.create())
-        model_config['params']['unet_config']['params']['use_checkpoint']=False   
-        model_list = []
-        for gpu_id in range(gpu_num):
-            model = instantiate_from_config(model_config)
-            # model = model.cuda(gpu_id)
-            print(ckpt_path)
-            assert os.path.exists(ckpt_path), "Error: checkpoint Not Found!"
-            model = load_model_checkpoint(model, ckpt_path)
-            model.eval()
-            model_list.append(model)
-        self.model_list = model_list
+        model_config['params']['unet_config']['params']['use_checkpoint'] = False   
+        
+        model = instantiate_from_config(model_config)
+        print(ckpt_path)
+        assert os.path.exists(ckpt_path), "Error: checkpoint Not Found!"
+        model = load_model_checkpoint(model, ckpt_path)
+        model.eval()
+        
+        # Wrap the model with DataParallel
+        self.model = torch.nn.DataParallel(model)
+        self.model = self.model.cuda()
+        
         self.save_fps = 8
 
     def get_image(self, image, prompt, steps=50, cfg_scale=7.5, eta=1.0, fs=3, seed=123, image2=None):
@@ -40,56 +41,49 @@ class Image2Video():
         transform = transforms.Compose([
             transforms.Resize(min(self.resolution)),
             transforms.CenterCrop(self.resolution),
-            ])
+        ])
         torch.cuda.empty_cache()
-        print('start:', prompt, time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+        print('start:', prompt, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
         start = time.time()
-        gpu_id=0
+        
         if steps > 60:
             steps = 60 
-        model = self.model_list[gpu_id]
-        model = model.cuda()
-        batch_size=1
-        channels = model.model.diffusion_model.out_channels
-        frames = model.temporal_length
+        model = self.model
+        batch_size = 1
+        channels = model.module.model.diffusion_model.out_channels
+        frames = model.module.temporal_length
         h, w = self.resolution[0] // 8, self.resolution[1] // 8
         noise_shape = [batch_size, channels, frames, h, w]
 
         # text cond
         with torch.no_grad(), torch.cuda.amp.autocast():
-            text_emb = model.get_learned_conditioning([prompt])
+            text_emb = model.module.get_learned_conditioning([prompt])
 
             # img cond
             img_tensor = torch.from_numpy(image).permute(2, 0, 1).float().to(model.device)
             img_tensor = (img_tensor / 255. - 0.5) * 2
 
-            image_tensor_resized = transform(img_tensor) #3,h,w
+            image_tensor_resized = transform(img_tensor) # 3,h,w
             videos = image_tensor_resized.unsqueeze(0).unsqueeze(2) # bc1hw
             
-            # z = get_latent_z(model, videos) #bc,1,hw
             videos = repeat(videos, 'b c t h w -> b c (repeat t) h w', repeat=frames//2)
             
-            
-
-
             img_tensor2 = torch.from_numpy(image2).permute(2, 0, 1).float().to(model.device)
             img_tensor2 = (img_tensor2 / 255. - 0.5) * 2
-            image_tensor_resized2 = transform(img_tensor2) #3,h,w
+            image_tensor_resized2 = transform(img_tensor2) # 3,h,w
             videos2 = image_tensor_resized2.unsqueeze(0).unsqueeze(2) # bchw
             videos2 = repeat(videos2, 'b c t h w -> b c (repeat t) h w', repeat=frames//2)
             
-            
             videos = torch.cat([videos, videos2], dim=2)
-            z, hs = self.get_latent_z_with_hidden_states(model, videos)
+            z, hs = self.get_latent_z_with_hidden_states(model.module, videos)
 
             img_tensor_repeat = torch.zeros_like(z)
 
             img_tensor_repeat[:,:,:1,:,:] = z[:,:,:1,:,:]
             img_tensor_repeat[:,:,-1:,:,:] = z[:,:,-1:,:,:]
 
-
-            cond_images = model.embedder(img_tensor.unsqueeze(0)) ## blc
-            img_emb = model.image_proj_model(cond_images)
+            cond_images = model.module.embedder(img_tensor.unsqueeze(0)) ## blc
+            img_emb = model.module.image_proj_model(cond_images)
 
             imtext_cond = torch.cat([text_emb, img_emb], dim=1)
 
@@ -97,7 +91,7 @@ class Image2Video():
             cond = {"c_crossattn": [imtext_cond], "fs": fs, "c_concat": [img_tensor_repeat]}
             
             ## inference
-            batch_samples = batch_ddim_sampling(model, cond, noise_shape, n_samples=1, ddim_steps=steps, ddim_eta=eta, cfg_scale=cfg_scale, hs=hs)
+            batch_samples = batch_ddim_sampling(model.module, cond, noise_shape, n_samples=1, ddim_steps=steps, ddim_eta=eta, cfg_scale=cfg_scale, hs=hs)
 
             ## remove the last frame
             if image2 is None:
@@ -105,7 +99,7 @@ class Image2Video():
             ## b,samples,c,t,h,w
             prompt_str = prompt.replace("/", "_slash_") if "/" in prompt else prompt
             prompt_str = prompt_str.replace(" ", "_") if " " in prompt else prompt_str
-            prompt_str=prompt_str[:40]
+            prompt_str = prompt_str[:40]
             if len(prompt_str) == 0:
                 prompt_str = 'empty_prompt'
 
@@ -117,12 +111,12 @@ class Image2Video():
     def download_model(self):
         REPO_ID = 'Doubiiu/ToonCrafter'
         filename_list = ['model.ckpt']
-        if not os.path.exists('./checkpoints/tooncrafter_'+str(self.resolution[1])+'_interp_v1/'):
-            os.makedirs('./checkpoints/tooncrafter_'+str(self.resolution[1])+'_interp_v1/')
+        if not os.path.exists('./checkpoints/tooncrafter_' + str(self.resolution[1]) + '_interp_v1/'):
+            os.makedirs('./checkpoints/tooncrafter_' + str(self.resolution[1]) + '_interp_v1/')
         for filename in filename_list:
-            local_file = os.path.join('./checkpoints/tooncrafter_'+str(self.resolution[1])+'_interp_v1/', filename)
+            local_file = os.path.join('./checkpoints/tooncrafter_' + str(self.resolution[1]) + '_interp_v1/', filename)
             if not os.path.exists(local_file):
-                hf_hub_download(repo_id=REPO_ID, filename=filename, local_dir='./checkpoints/tooncrafter_'+str(self.resolution[1])+'_interp_v1/', local_dir_use_symlinks=False)
+                hf_hub_download(repo_id=REPO_ID, filename=filename, local_dir='./checkpoints/tooncrafter_' + str(self.resolution[1]) + '_interp_v1/', local_dir_use_symlinks=False)
     
     def get_latent_z_with_hidden_states(self, model, videos):
         b, c, t, h, w = videos.shape
@@ -139,7 +133,8 @@ class Image2Video():
         z = model.get_first_stage_encoding(encoder_posterior).detach()
         z = rearrange(z, '(b t) c h w -> b c t h w', b=b, t=t)
         return z, hidden_states_first_last
+
 if __name__ == '__main__':
     i2v = Image2Video()
-    video_path = i2v.get_image('prompts/art.png','man fishing in a boat at sunset')
+    video_path = i2v.get_image('prompts/art.png', 'man fishing in a boat at sunset')
     print('done', video_path)
